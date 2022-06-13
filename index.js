3c92f53b25e11645086dd0f2dda0ed3b3d6cba11
MayankD <mayank.d@coinswitch.co>
2022-06-13 22:28:24 +0530

    Comment added for the order

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // initiaziling express 
+
 // Module initiazilination START
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs'); // it help us in create or writing file in local machine
+
 // Module initiazilination END
 
 const app = express(); // Invoking express
 
-
-
 const port = 3000; // System port 3000
 
+// Middleware START
 app.use(bodyParser.json()) // use - for Every Request app.use will invoke
 
 app.use((req,res,next)=>{
     console.log(req.url);
     next(); // this will go to the
 })
-// ROUTE 
+
+// Middleware END
+
+
+
+// ROUTE  START
 
 app
   .route("/article")
@@ -35,13 +41,18 @@ app
     res.json(data);
   }) // Here we will retrive/get POST by id
   
+// Middleware END
 
 
+// Forget about this code
   const writeFile = (fileName,content) => {
     fs.writeFile(`${fileName}.json`,content, (err)=> {
         if (err) console.log(err);
     })
   }
+
+  // Forget about this code
+
 
 
 app.listen(port, () => {
