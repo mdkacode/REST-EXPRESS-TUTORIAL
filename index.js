e4339a49b16183335afc3f33fc7e723435cda2fa
MayankD <mayank.d@coinswitch.co>
2022-06-13 22:22:22 +0530

    API Endpoints

---
+++ index.js
@@ -0,0 +1,49 @@
+// initiaziling express 
+// Module initiazilination START
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs'); // it help us in create or writing file in local machine
+// Module initiazilination END
+
+const app = express(); // Invoking express
+
+
+
+const port = 3000; // System port 3000
+
+app.use(bodyParser.json()) // use - for Every Request app.use will invoke
+
+app.use((req,res,next)=>{
+    console.log(req.url);
+    next(); // this will go to the
+})
+// ROUTE 
+
+app
+  .route("/article")
+  .post((req,res) => {
+    console.log(req.query);
+    writeFile('notes',JSON.stringify(req.body)); // this create a file called notes or not
+    res.send("POST Created")
+  }) // Here we will create POST
+  .get((req,res) => {
+    const data = {
+        "id": 1,
+        "title": "Sample Code",
+        "content": "<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>"
+    }
+    res.json(data);
+  }) // Here we will retrive/get POST by id
+  
+
+
+  const writeFile = (fileName,content) => {
+    fs.writeFile(`${fileName}.json`,content, (err)=> {
+        if (err) console.log(err);
+    })
+  }
+
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
